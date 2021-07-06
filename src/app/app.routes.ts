5f4ddc6f1570d4841786018c61a6bc6fae7ad2b1
Breno Robert <64442888+BrenoRobertDiogo@users.noreply.github.com>
2021-07-06 15:14:50 -0300

    Funcionamento entre abas e inserção banco de dados na API com mais dados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,26 +4,42 @@ import { HomeComponent } from "./home/home.component";
 import { RestaurantesComponent } from "./restaurantes/restaurantes.component";
 import { DiversaoComponent } from "./diversao/diversao.component";
 import { OfertaComponent } from "./oferta/oferta.component";
+import { ComoUsarComponent } from "./oferta/como-usar/como-usar.component";
+import { OndeFicaComponent } from "./oferta/onde-fica/onde-fica.component";
 
 export const ROUTES: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    },
-    {
-        path: 'Restaurantes',
-        component: RestaurantesComponent,
-    },
-    {
-        path: 'Diversao',
-        component: DiversaoComponent,
-    },
-    {
-        path: 'Oferta',
-        component: OfertaComponent
-    },
-    {
-        path: 'Oferta/:id',
-        component: OfertaComponent
-    }
-]
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "Restaurantes",
+    component: RestaurantesComponent,
+  },
+  {
+    path: "Diversao",
+    component: DiversaoComponent,
+  },
+  {
+    path: "Oferta",
+    component: OfertaComponent,
+  },
+  {
+    path: "Oferta/:id",
+    component: OfertaComponent,
+    children: [
+      {
+        path: "",
+        component: ComoUsarComponent,
+      },
+      {
+        path: "como-usar",
+        component: ComoUsarComponent,
+      },
+      {
+        path: "onde-fica",
+        component: OndeFicaComponent,
+      },
+    ],
+  },
+];
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -5,62 +5,13 @@ import { Injectable } from "@angular/core";
 
 import 'rxjs/add/operator/toPromise';
 
-@Injectable()
+import { API } from "./app.api";
+
+@Injectable() 
 export class OfertasService {
     
-    private api: string = 'http://localhost:3000/ofertas';
+    private api: string = API;
     constructor(private http: Http) { }
-
-    public ofertas: Array<Oferta> = [
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                { url: "/assets/ofertas/1/img1.jpg" },
-                { url: "/assets/ofertas/1/img2.jpg" },
-                { url: "/assets/ofertas/1/img3.jpg" },
-                { url: "/assets/ofertas/1/img4.jpg" }
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                { url: "/assets/ofertas/2/img1.jpg" },
-                { url: "/assets/ofertas/2/img2.jpg" },
-                { url: "/assets/ofertas/2/img3.jpg" },
-                { url: "/assets/ofertas/2/img4.jpg" }
-            ]
-
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Estância das águas",
-            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                { url: "/assets/ofertas/3/img1.jpg" },
-                { url: "/assets/ofertas/3/img2.jpg" },
-                { url: "/assets/ofertas/3/img3.jpg" },
-                { url: "/assets/ofertas/3/img4.jpg" },
-                { url: "/assets/ofertas/3/img5.jpg" },
-                { url: "/assets/ofertas/3/img6.jpg" }
-            ]
-        }
-    ];
 
     /**
      * getOfertas
