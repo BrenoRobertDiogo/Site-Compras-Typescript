255bfc7e60566c820313f5032182382650e6b73e
Breno Robert <64442888+BrenoRobertDiogo@users.noreply.github.com>
2021-07-07 11:48:15 -0300

    Implementação rxjs

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,29 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { OfertasService } from 'app/ofertas.service';
-import { Oferta } from 'app/shared/oferta.model';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { OfertasService } from "app/ofertas.service";
+import { Oferta } from "app/shared/oferta.model";
+import { Observable } from "rxjs/Observable";
+import "rxjs";
+import { Observer, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-oferta',
-  templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css'],
-  providers: [OfertasService]
+  selector: "app-oferta",
+  templateUrl: "./oferta.component.html",
+  styleUrls: ["./oferta.component.css"],
+  providers: [OfertasService],
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
   public oferta: Oferta;
-  constructor(private route: ActivatedRoute, private ofertasService: OfertasService) {  }
+  private tempoObservableSubscription: Subscription 
+  private tempoObservableTesteSubscription: Subscription 
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
+  
 
   ngOnInit() {
-    let id: any = this.route.snapshot.params['id'];
-    this.ofertasService.getOfertasPorId(id)
-    .then((oferta: Oferta) => {
+    let id: any = this.route.snapshot.params["id"];
+    this.ofertasService.getOfertasPorId(id).then((oferta: Oferta) => {
       this.oferta = oferta;
-    })
-    /* this.route.params.subscribe((parametro: any) => {
-      console.log(parametro);
-      
-    }) */
-     
-  }
+    });
+    this.route.params.subscribe(
+      (parametro: any) => {
+        console.log(parametro);
+      },
+      (erro: any) => {
+        console.log(erro);
+      },
+      () => {
+        console.log("Acabou o processamento");
+      }
+    );
 
+    let tempo = Observable.interval(2000); // A cada 2s vai ter um observável
+
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
+      console.log(intervalo);
+    });
+    // observable(observavel)
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(3);
+      observer.error("Algum erro foi encontrado na stream de eventos");
+      observer.next(3);
+    });
+
+    // observable(observador)
+    this.tempoObservableTesteSubscription =  meuObservableTeste.subscribe(
+      (resultado: any) => console.log(resultado), // NEXT
+      (error: string) => console.log(error), // ERRO
+      () => console.log("Stream de eventos finalizada") // Complete
+    );
+  } 
+  ngOnDestroy(): void {
+    this.tempoObservableSubscription.unsubscribe();
+    this.tempoObservableTesteSubscription.unsubscribe();
+  }
 }
